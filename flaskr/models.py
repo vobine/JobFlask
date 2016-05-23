5fd05f03f8b413a4fbf4a1b95e8beb4ba2160abd
Hal Peterson <vobine@users.noreply.github.com>
2016-05-23 15:30:16 -0500

    It works!

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,4 +1,4 @@
-import sqlalchemy as sqla
+import sqlalchemy as sql
 import sqlalchemy.orm as orm
 import sqlalchemy.ext.declarative as sqldcl
 
@@ -8,9 +8,23 @@ class Entries (Base):
     """FlaskR entry definition"""
     __tablename__ = 'entries'
 
-    id = sqla.Column (sqla.Integer, primary_key=True)
-    title = sqla.Column (sqla.String (80))
-    text = sqla.Column (sqla.Text)
+    id = sql.Column (sql.Integer, primary_key=True)
+    title = sql.Column (sql.String (80))
+    text = sql.Column (sql.Text)
+
+    def __init__ (self, title, text):
+        self.title = title
+        self.text = text
 
     def __repr__ (self):
         return '<Entry {0:d}: {1:s}>'.format (self.id, self.title)
+
+def init_db (url='sqlite:///:memory:', verbose=False):
+    """Initialize connection to database."""
+    engine = sql.create_engine (url, echo=verbose)
+    session = orm.scoped_session (
+        orm.sessionmaker (autocommit=False,
+                          autoflush=False,
+                          bind=engine))
+    Base.query = session.query_property ()
+    Base.metadata.create_all (bind=engine)
