439d74647e7e9f5c84c8cbb83accef2bd3759559
Hal Peterson <vobine@users.noreply.github.com>
2016-06-05 21:52:31 -0500

    Give the command-line function a proper (if skeletal) CLI.

--- jobflask.py
+++ jobflask.py
@@ -5,7 +5,15 @@ def run (debug):
     app.run (debug=debug)
 
 def main (argv):
-    run (True)
+    """CLI."""
+    parser = argparse.ArgumentParser (
+        'JobFlask: an implementation of JobJar using Flask.')
+    parser.add_argument ('--debug', '-d',
+                         action='store_true',
+                         help='Run server in debug mode')
+
+    args = parser.parse_args (argv)
+    run (args.debug)
 
 if __name__ == '__main__':
     from sys import argv
