08661f2d56d9eff4d125b70938ae99b759deb957
Hal Peterson <vobine@users.noreply.github.com>
2016-06-14 09:56:37 -0500

    Initial copy of models from Django. Broken!

--- jobflask/models.py
+++ jobflask/models.py
@@ -14,3 +14,65 @@ def init_db (url, verbose=False):
                           bind=engine))
     Base.query = session.query_property ()
     Base.metadata.create_all (bind=engine)
+
+
+# Implementation parameters.
+CHAR_LIMITS = {
+    'abbr' : 4,
+    'name' : 20,
+    'desc' : 120,
+}
+
+JOB_STATES_LIST = ((1, 'new', 'Uninitialized',
+                    'Created but not yet fully fleshed out' ),
+                   (2, 'busy', 'Busy',
+                    'Currently in progress' ),
+                   (3, 'wait', 'Available',
+                    'Available for scheduling' ),
+                   (4, 'idle', 'Idle',
+                    'Not complete, but not ready either' ),
+                   (5, 'done', 'Completed',
+                    'No more work to be done' ),
+                   (6, 'what', 'Unknown',
+                    'State unknown, probably a bug' ), )
+JOB_STATES = dict ((jsv[0], jsv[1:])
+                   for jsv in JOB_STATES_LIST)
+JOB_STATES_ABBR = dict ((v[0], k)
+                        for k, v in JOB_STATES.items ())
+
+# We subclass User to turn it into something more flexible someday.
+class JobOwner (Base):
+    """This will include some more elaborate permission mechanism."""
+    pass
+
+# All models have a default ID field.
+
+class Job (Base):
+    """An individual task to be scheduled. Grist for the mill."""
+    name = models.CharField (max_length=CHAR_LIMITS['name'])
+    desc = models.CharField (max_length=CHAR_LIMITS['desc'])
+    owner = models.ForeignKey (JobOwner, null=True)
+    state = models.IntegerField (choices=tuple (
+        (k, v[1]) for k, v in JOB_STATES.items ()))
+
+    def __unicode__ (self):
+        """Text representation."""
+        return '{0:s}'.format (self.name)
+
+class JobEventType (Base):
+    """A classification of events in the Log."""
+    abbrev = models.CharField (max_length=CHAR_LIMITS['abbr'])
+    name = models.CharField (max_length=CHAR_LIMITS['name'])
+    desc = models.CharField (max_length=CHAR_LIMITS['desc'])
+
+    def __unicode__ (self):
+        """Text representation."""
+        return '{0:5s} {1:s}'.format (self.abbrev, self.name)
+
+class JobLog (Base):
+    """A state change for a Job."""
+    job = models.ForeignKey (Job)
+    owner = models.ForeignKey (JobOwner, null=True)
+    event = models.ForeignKey (JobEventType)
+    timestamp = models.DateTimeField (auto_now_add=True)
+    note = models.CharField (max_length=CHAR_LIMITS['desc'])
